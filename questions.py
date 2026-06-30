6a6c21463a0e38e5390615914d39a9f4c7b61cda
Thecanadianchris <thecanadianchris@yahoo.com>
2026-06-30 09:00:26 +0100

    Code start

--- questions.py
+++ questions.py
@@ -2,3 +2,15 @@ from InquirerPy import inquirer
 
 
 class Questions:
+
+    def ask_all(self):
+        
+        answers = {}
+
+    answers["title"] = inquirer.text(
+            message="Project title:"
+        ).execute()   
+    
+    answers["description"] = inquirer.text(
+            message="Short description of the project:"
+        ).execute()

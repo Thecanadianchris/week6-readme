155ab5c18bf84a4217aec3547b93c61e7c1aad3e
Thecanadianchris <thecanadianchris@yahoo.com>
2026-06-30 09:06:33 +0100

    added more questions and list

--- questions.py
+++ questions.py
@@ -3,14 +3,35 @@ from InquirerPy import inquirer
 
 class Questions:
 
+    LICENSE_OPTIONS = ["Python", "Javasript", "BootStrap", "Other", "None"]
+
+
+
     def ask_all(self):
         
         answers = {}
 
-    answers["title"] = inquirer.text(
+        answers["title"] = inquirer.text(
             message="Project title:"
         ).execute()   
+
     
-    answers["description"] = inquirer.text(
+        answers["description"] = inquirer.text(
             message="Short description of the project:"
+        ).execute()
+
+
+        answers["installation"] = inquirer.text(
+            message="Installation instructions:"
+        ).execute()
+
+
+        answers["usage"] = inquirer.text(
+            message="Usage information:"
+        ).execute()
+
+
+        answers["license"] = inquirer.select(
+            message="Choose a license:",
+            choices=self.LICENSE_OPTIONS,
         ).execute()
